3a3dc7db68a0dfddc33fb95102068f7d4afb99e6
Alassane NIANG <adniang75@gmail.com>
2022-08-12 03:35:36 +0200

    Initial commit

---
+++ src/main/java/com/alassaneniang/hanselandpetal/TddWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.hanselandpetal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alassaneniang/hanselandpetal/TddWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.hanselandpetal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
